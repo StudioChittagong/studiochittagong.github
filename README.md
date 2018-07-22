0baf92fe8c0c999826a419c76dffef849eb3e334
sayemon10 <abdulaziz.aziz99@hmail.com>
2018-07-22 19:28:00 +0600

    version_001

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # studiochittagong.github.io
-Studio chittagong is a “human infrastructure of skilled craftsmen and architects who design and build the work directly.
+Studio chittagong is a human infrastructure of skilled craftsmen and architects who design and build the work directly.
---
+++ scripts/script.js
@@ -0,0 +1,25 @@
+// executes when HTML-Document is loaded and DOM is ready
+
+
+/*
+################
+Add navbar background color when scrolled
+*/
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 30) {
+        $(".navbar").addClass("NavbarBg");
+    } else {
+        $(".navbar").removeClass("NavbarBg");
+    }
+});
+// If Mobile, add background color when toggler is clicked
+$(".navbar-toggler").click(function () {
+    if (!$(".navbar-collapse").hasClass("show")) {
+        $(".navbar").addClass("NavbarBg");
+    } else {
+        if ($(window).scrollTop() < 56) {
+            $(".navbar").removeClass("NavbarBg");
+        } else {
+        }
+    }
+});
