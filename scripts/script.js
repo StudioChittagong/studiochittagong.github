0f9fc2eb7a1913ecffe9366d2d21bfd086165cbe
sayemon10 <abdulaziz.aziz99@hmail.com>
2018-12-03 20:03:37 +0600

    js modifiv

--- scripts/script.js
+++ scripts/script.js
@@ -41,7 +41,7 @@ $(function () {
     $("li.list-inline-item").on('click', function () {
         selectedClass = $(this).attr("data-rel");
         $("#portfolio").fadeTo(100, 0.1);
-        $("#portfolio div").not("." + selectedClass).fadeOut().removeClass('scale-anm');
+        $("#portfolio>div").not("." + selectedClass).fadeOut().removeClass('scale-anm');
         setTimeout(function () {
             $("." + selectedClass).fadeIn().addClass('scale-anm');
             $("#portfolio").fadeTo(300, 1);
