b6e80a6cdf78992119cce41fa2a698c9e78a2d80
sayemon10 <abdulaziz.aziz99@hmail.com>
2018-12-03 19:45:40 +0600

    project classification

--- scripts/script.js
+++ scripts/script.js
@@ -26,11 +26,25 @@ $(".navbar-toggler").click(function () {
 
 //Preloader
 var preloader = $('.preloader');
-$(window).on('load', function() {
+$(window).on('load', function () {
     var preloaderFadeOutTime = 500;
 
     function hidePreloader() {
         preloader.fadeOut(preloaderFadeOutTime);
     }
     hidePreloader();
+});
+
+// Project classification
+$(function () {
+    var selectedClass = "";
+    $("li.list-inline-item").on('click', function () {
+        selectedClass = $(this).attr("data-rel");
+        $("#portfolio").fadeTo(100, 0.1);
+        $("#portfolio div").not("." + selectedClass).fadeOut().removeClass('scale-anm');
+        setTimeout(function () {
+            $("." + selectedClass).fadeIn().addClass('scale-anm');
+            $("#portfolio").fadeTo(300, 1);
+        }, 300);
+    });
 });
